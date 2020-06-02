b91d35b58cd97cf0ebb02cd0b279e3b6e0db3f9c
Josh Hooker <joshualeehooker@gmail.com>
2020-06-02 14:49:28 -0400

    Updates

---
+++ precache-manifest.3ddc56a6590491f87a150575940e7dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49f1b0137572660497fe3521156b76e",
+    "url": "/Simple_Kinematics_Calculator/index.html"
+  },
+  {
+    "revision": "e162cf34ecb5fa58f8fd",
+    "url": "/Simple_Kinematics_Calculator/static/css/main.c3a469a2.chunk.css"
+  },
+  {
+    "revision": "0da24edadaf7eca74d29",
+    "url": "/Simple_Kinematics_Calculator/static/js/2.2352ff03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple_Kinematics_Calculator/static/js/2.2352ff03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e162cf34ecb5fa58f8fd",
+    "url": "/Simple_Kinematics_Calculator/static/js/main.696f85a5.chunk.js"
+  },
+  {
+    "revision": "211a28e4037ec97458ae",
+    "url": "/Simple_Kinematics_Calculator/static/js/runtime-main.5a925543.js"
+  }
+]);
